8cd0bb4cab0fb040e2f4e8eb65fb85ec64591289
asnabryg <8mixu8@gmail.com>
2021-07-25 17:17:41 +0300

    3.21

--- index.js
+++ index.js
@@ -72,16 +72,6 @@ app.get("/api/persons/:id", (req, res, next) => {
 app.post("/api/persons", (req, res, next) => {
     const body = req.body
     console.log(body)
-    if (!body.name){
-        return res.status(400).json({
-            error:"name missing"
-        })
-    }
-    if (!body.number) {
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    }
     const person = new Person({
         name: body.name,
         number: body.number
@@ -98,7 +88,7 @@ app.put("/api/persons/:id", (req, res, next) => {
         name: body.name,
         number: body.number
     }
-    Person.findByIdAndUpdate(req.params.id, person, {new: true}).then(updatedPerson => {
+    Person.findByIdAndUpdate(req.params.id, person, {new: true, runValidators: true, context: "query"}).then(updatedPerson => {
         res.json(updatedPerson)
     }).catch(error => next(error))
 })
@@ -113,6 +103,8 @@ const errorHandler = (error, req, res, next) => {
     console.log(error.message)
     if (error.name === "CastError") {
         return res.status(400).send({error: "mailformatted id"})
+    }else if (error.name === "ValidationError") {
+        return res.status(400).json({error: error.message})
     }
     next(error)
 }
